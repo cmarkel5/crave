ddce38475c34c4cffced5b951f0b7834c504d4fd
cmarkel5 <cmarkel5@gmail.com>
2014-12-13 14:49:27 -0500

    shop page working

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,24 @@ class Product < ActiveRecord::Base
     walmart_api_key = "x4bsdrczpgjsh8xyk7s5ssda"
     category = "electronics"
     search_url = "http://api.walmartlabs.com/v1/search?apiKey=#{walmart_api_key}&query=#{category}&facet=on&facet.range=price:[0%20TO%2050]"
-    JSON.create_id = nil
+    # JSON.create_id = nil
     response = HTTParty.get search_url
     response["items"]
   end
 end
+
+  # def self.random
+  #   cal_quote = self.grab_quote
+  #   until cal_quote["source"] == "calvin" do 
+  #     cal_quote = self.grab_quote
+  #   end
+  #   return cal_quote
+  # end
+
+  # private
+  #   def self.grab_quote 
+  #     source = "calvin"
+  #     search_url = "http://www.iheartquotes.com/api/v1/random.json?source=#{source}"
+  #     JSON.create_id = nil
+  #     return HTTParty.get search_url
+  #   end
--- app/views/products/electronics.json.jbuilder
+++ app/views/products/electronics.json.jbuilder
@@ -1,4 +1,11 @@
 json.array!(@electronics) do |electronic|
-  json.name electronic["name"]
-  json.customerRating electronic["customerRating"]
+  json.itemId               electronic["itemId"]
+  json.name                 electronic["name"]
+  json.msrp                 electronic["msrp"]
+  json.salePrice            electronic["salePrice"]
+  json.thumbnailImage       electronic["thumbnailImage"]
+  json.shortDescription     electronic["shortDescription"]
+  json.customerRatingImage  electronic["customerRatingImage"]
 end
+
+  
