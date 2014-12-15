15479f90452ad96b185b9c042a29fe176f4c293f
cmarkel5 <cmarkel5@gmail.com>
2014-12-15 14:10:50 -0500

    purchases assigned to customer

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,6 @@ class Product < ActiveRecord::Base
     walmart_api_key = "x4bsdrczpgjsh8xyk7s5ssda"
     category = "electronics"
     search_url = "http://api.walmartlabs.com/v1/search?apiKey=#{walmart_api_key}&query=#{category}&facet=on&facet.range=price:[0%20TO%2050]"
-    # JSON.create_id = nil
     response = HTTParty.get search_url
     response["items"]
   end
