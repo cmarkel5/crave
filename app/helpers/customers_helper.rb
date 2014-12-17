4e28484644db06d5e42d445343ea620d9925db51
cmarkel5 <cmarkel5@gmail.com>
2014-12-17 07:46:29 -0500

    some styling done

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -3,6 +3,6 @@ module CustomersHelper
   def gravatar_for(customer)
     gravatar_id = Digest::MD5::hexdigest(customer.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: customer.name, class: "gravatar")
+    image_tag(gravatar_url, alt: customer.name, class: "gravatar img-circle")
   end
 end
